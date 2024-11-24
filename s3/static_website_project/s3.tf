4fcaf94abe83c9ab0fe4c055d237f50d17330515
AaronHapy <aaroncerceda26@gmail.com>
2024-11-24 00:05:36 -0600

    allow public access, configuring website and upload files

--- s3/static_website_project/s3.tf
+++ s3/static_website_project/s3.tf
@@ -3,5 +3,63 @@ resource "random_id" "bucket_suffix" {
 }
 
 resource "aws_s3_bucket" "static_website" {
+  # Deploy bucket to s3
   bucket = "static-content-terraform-${random_id.bucket_suffix.hex}"
+}
+
+# Disabling Public Access Block
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket                  = aws_s3_bucket.static_website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+  bucket = aws_s3_bucket.static_website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+# Upload objects to S3 Bucket
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  source       = "build/index.html"
+  etag         = filemd5("build/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "error.html"
+  source       = "build/error.html"
+  etag         = filemd5("build/error.html")
+  content_type = "text/html"
 }
